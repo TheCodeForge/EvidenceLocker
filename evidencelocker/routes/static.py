0cb214d0bf594293880f492b487880eb3618ffe1
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-02-21 16:10:07 -0500

    home

--- evidencelocker/routes/static.py
+++ evidencelocker/routes/static.py
@@ -3,6 +3,16 @@ from flask import *
 
 from evidencelocker.__main__ import app
 
+@app.get('/')
+def home():
+    return render_template(
+        "home.html"
+        )
+
+@app.get("/help/<pagename>")
+def help(pagename):
+	return render_template(safe_join("/help", pagename)+'.html')
+
 @app.get("/assets/style/light.css")
 def light_css():
 	with open('evidencelocker/assets/style/light.scss') as stylesheet:
