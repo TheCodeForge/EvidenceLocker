16647ad4a839a71325c3bf38ccfbabe2ab0d97dd
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-02-21 11:05:58 -0500

    fix filename

--- evidencelocker/routes/static.py
+++ evidencelocker/routes/static.py
@@ -4,5 +4,5 @@ from evidencelocker.__main__ import app
 
 @app.get("/assets/style/light.css")
 def light_css():
-	with open('evidencelocker/evidencelocker/assets/style/light.scss') as stylesheet:
+	with open('~/evidencelocker/assets/style/light.scss') as stylesheet:
 		return Response(sass.compile(string=stylesheet.read(), mimetype="text/css"))
