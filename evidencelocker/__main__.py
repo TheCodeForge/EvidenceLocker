54b1dc4918d6e08b76104a0b10adcd14e3fe5a92
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-02-21 13:19:22 -0500

    Update __main__.py

--- evidencelocker/__main__.py
+++ evidencelocker/__main__.py
@@ -19,7 +19,7 @@ app=Flask(
 app.url_map.strict_slashes=False
 
 #===CONFIGS===
-app.config['DATABASE_URL']                  = environ.get("DATABASE_URL", input("Database URL: ")).replace("postgres://", "postgresql://")
+app.config['DATABASE_URL']                  = environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
 app.config["PERMANENT_SESSION_LIFETIME"]    = 60 * 60
 app.config["SESSION_REFRESH_EACH_REQUEST"]  = True
 app.config['SECRET_KEY']                    = environ.get("SECRET_KEY")
