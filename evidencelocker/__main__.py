5e9ad5d5711b3b9be8a7f3ec9cd9ff2561f78340
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-02-21 13:39:06 -0500

    now testing for real

--- evidencelocker/__main__.py
+++ evidencelocker/__main__.py
@@ -43,12 +43,11 @@ from .routes import *
 #===ALEMBIC DB SCHEMA UPDATE===
 from alembic.config import Config
 from alembic import command
-def run_migrations(script_location: str, dsn: str) -> None:
-    #LOG.info('Running DB migrations in %r on %r', script_location, dsn)
-    alembic_cfg = Config()
-    alembic_cfg.set_main_option('sqlalchemy.url', app.config["DATABASE_URL"])
-    command.revision(alembic_cfg, autogenerate=True)
-    command.upgrade(alembic_cfg, 'head')
+#LOG.info('Running DB migrations in %r on %r', script_location, dsn)
+alembic_cfg = Config()
+alembic_cfg.set_main_option('sqlalchemy.url', app.config["DATABASE_URL"])
+command.revision(alembic_cfg, autogenerate=True)
+command.upgrade(alembic_cfg, 'head')
 
 @app.before_request
 def before_request():
