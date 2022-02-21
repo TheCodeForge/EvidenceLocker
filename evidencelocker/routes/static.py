276b9b572527f5366567bdb0d9cd415884d7e133
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-02-21 11:27:36 -0500

    Update static.py

--- evidencelocker/routes/static.py
+++ evidencelocker/routes/static.py
@@ -1,8 +1,14 @@
 import sass
+from flask import *
 
 from evidencelocker.__main__ import app
 
 @app.get("/assets/style/light.css")
 def light_css():
 	with open('evidencelocker/assets/style/light.scss') as stylesheet:
-		return Response(sass.compile(string=stylesheet.read(), mimetype="text/css"))
+		return Response(
+			sass.compile(
+				string=stylesheet.read(), 
+				mimetype="text/css"
+				)
+			)
