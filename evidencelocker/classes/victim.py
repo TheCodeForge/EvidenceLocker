7da989adea6bf18583448c1940d1709b6bfeb270
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-02-22 14:01:41 -0500

    victim properties

--- evidencelocker/classes/victim.py
+++ evidencelocker/classes/victim.py
@@ -1,11 +1,19 @@
 from sqlalchemy import *
 from sqlalchemy.orm import relationship, lazyload
 
+from .mixins import time_mixin
 from evidencelocker.__main__ import Base
 
-class VictimUser(Base):
+class VictimUser(Base, time_mixin):
 
     __tablename__="victim_users"
     
     id          =Column(Integer, primary_key=True)
+    username    =Column(String(64))
     created_utc =Column(Integer)
+    name        =Column(String(128))
+    country     =Column(String(2))
+    pw_hash     =Column(String(256))
+    otp_secret  =Column(String(32))
+
+
