798d5c6447eddbf88626def3b878d716ae9f0b7f
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-02 10:29:00 -0500

    Update loaders.py

--- evidencelocker/helpers/loaders.py
+++ evidencelocker/helpers/loaders.py
@@ -16,7 +16,7 @@ def get_victim_by_username(name, graceful=False):
 
 def get_victim_by_id(id, graceful=False):
 
-	if not isinstance(name, int):
+	if not isinstance(id, int):
 		raise TypeError("ID must be int")
 
 	user = g.db.query(Victim).filter_by(id=id).first()
@@ -32,6 +32,19 @@ def get_police_by_email(email, graceful=False):
 		raise TypeError("Police email must be str")
 
 	user = g.db.query(Police).filter_by(email=name).first()
+
+	if not user and not graceful:
+		abort(404)
+
+	return user
+
+
+def get_police_by_id(id, graceful=False):
+
+	if not isinstance(id, int):
+		raise TypeError("ID must be int")
+
+	user = g.db.query(Police).filter_by(id=id).first()
 
 	if not user and not graceful:
 		abort(404)
