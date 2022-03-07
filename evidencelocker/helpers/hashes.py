b3f5609d833ad29846a5ad3f8906d07c7f82c16c
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-07 18:09:22 -0500

    Update hashes.py

--- evidencelocker/helpers/hashes.py
+++ evidencelocker/helpers/hashes.py
@@ -8,7 +8,7 @@ def generate_hash(string):
 
     msg = bytes(string, "utf-16")
 
-    return hmac.new(key=bytes(environ.get("MASTER_KEY"), "utf-16"),
+    return hmac.new(key=bytes(environ.get("SECRET_KEY"), "utf-16"),
                     msg=msg,
                     digestmod='md5'
                     ).hexdigest()
