8555c0bde25ef71d9627f07169c480713a6fda66
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-07 21:04:14 -0500

    Update __main__.py

--- evidencelocker/__main__.py
+++ evidencelocker/__main__.py
@@ -71,7 +71,6 @@ def before_request():
 def after_request(resp):
     
     resp.headers["Content-Security-Policy"] = "default-src 'self'; script-src hcaptcha.com code.jquery.com cdn.jsdelivr.net ; object-src 'none'; style-src 'self'; img-src w3.org; media-src 'none'; frame-src newassets.hcaptcha.com; font-src 'none'; connect-src 'self'; form-action 'self'; object-types 'none';"
-    resp.headers["Cross-Origin-Embedder-Policy"] = "credentialless"
     resp.headers["Cross-Origin-Opener-Policy"] = "same-origin"
     resp.headers["Cross-Origin-Resource-Policy"] = "same-origin"
     resp.headers["Permissions-Policy"] = "geolocation=(self)"
