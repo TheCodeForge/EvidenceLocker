bf64b6032f32432148aaad385e5fdf9bfdd6855b
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-07 19:31:57 -0500

    Update __main__.py

--- evidencelocker/__main__.py
+++ evidencelocker/__main__.py
@@ -22,6 +22,20 @@ app=Flask(
 
 app.url_map.strict_slashes=False
 
+#===CONFIGS===
+app.config['DATABASE_URL']                  = environ.get("DATABASE_URL",'').replace("postgres://", "postgresql://")
+if not app.config['DATABASE_URL']:
+    cfg=alembic.config.Config('alembic.ini')
+    app.config['DATABASE_URL']=cfg.get_main_option('sqlalchemy.url')
+    del cfg
+
+app.config["HCAPTCHA_SECRET"]               = environ.get("HCAPTCHA_SECRET")
+app.config["HCAPTCHA_SITEKEY"]              = environ.get("HCAPTCHA_SITEKEY")
+app.config["PERMANENT_SESSION_LIFETIME"]    = 60 * 60
+app.config["SESSION_REFRESH_EACH_REQUEST"]  = True
+app.config['SECRET_KEY']                    = environ.get("SECRET_KEY")
+app.config['SERVER_NAME']                   = environ.get("SERVER_NAME")
+
 #===SQLALCHEMY===
 _engine=create_engine(
     app.config['DATABASE_URL'],
@@ -38,29 +52,9 @@ Base=declarative_base()
 
 from .routes import *
 
-
-#===CONFIGS===
-app.config['DATABASE_URL']                  = environ.get("DATABASE_URL",'').replace("postgres://", "postgresql://")
-if not app.config['DATABASE_URL']:
-    cfg=alembic.config.Config('alembic.ini')
-    app.config['DATABASE_URL']=cfg.get_main_option('sqlalchemy.url')
-    del cfg
-
-app.config["HCAPTCHA_SECRET"]               = environ.get("HCAPTCHA_SECRET")
-app.config["HCAPTCHA_SITEKEY"]              = environ.get("HCAPTCHA_SITEKEY")
-app.config["PERMANENT_SESSION_LIFETIME"]    = 60 * 60
-app.config["SESSION_REFRESH_EACH_REQUEST"]  = True
-app.config['SECRET_KEY']                    = environ.get("SECRET_KEY")
-app.config['SERVER_NAME']                   = environ.get("SERVER_NAME")
-
-
 #===EXTENSIONS
 
 Markdown(app)
-
-
-
-
 
 #===BEFORE/AFTER REQS===
 
