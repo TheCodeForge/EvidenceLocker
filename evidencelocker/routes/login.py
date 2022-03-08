c63168d24a10b21ee871385d337ad3746de1b8e0
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-07 22:05:40 -0500

    invalid message

--- evidencelocker/routes/login.py
+++ evidencelocker/routes/login.py
@@ -57,7 +57,7 @@ def login_police():
     #Random sleep is to ensure timing analysis cannot be used to deduce which part of the login failed
     def invalid_login_police():
         time.sleep(max(0, random.gauss(1.5, 0.33)))
-        return redirect("/login_police?invalid")
+        return redirect("/login_police?invalid=1")
 
     user = get_police_by_email(request.form.get("email"), graceful=True)
     if not user:
