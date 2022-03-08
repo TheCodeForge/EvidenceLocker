b6b0b4ad7d1de413a96f6ccfaa9b6cbf324d9b74
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-07 19:19:04 -0500

    Update login.py

--- evidencelocker/routes/login.py
+++ evidencelocker/routes/login.py
@@ -5,7 +5,7 @@ import random
 import time
 import werkzeug.security
 
-from flask import session, g, redirect, render_template
+from flask import session, g, redirect, render_template, send_file
 
 from evidencelocker.helpers.loaders import *
 from evidencelocker.helpers.hashes import *
