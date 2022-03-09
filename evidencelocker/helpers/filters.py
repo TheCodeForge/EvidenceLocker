ae49f475148ae0630760e3c8fe743db47b384cf0
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-09 11:05:59 -0500

    Update filters.py

--- evidencelocker/helpers/filters.py
+++ evidencelocker/helpers/filters.py
@@ -6,19 +6,18 @@ from evidencelocker.__main__ import app
 @app.template_filter('qrcode_img_data')
 def qrcode_filter(x):
   
-    qr = qrcode.QRCode()
-    qr.add_data(x)
-  
     mem=io.BytesIO()
-    img=qr.make_image(
+    img=qrcode.make(
+        x,
         fill_color="#2589bd",
-        back_color="white"
+        back_color="white",
+        image_factory=qrcode.image.svg.SvgImage
     )
     img.save(
         mem, 
-        format="PNG"
+        format="SVG"
     )
     mem.seek(0)
     
     data=base64.b64encode(mem.read()).decode('ascii')
-    return f"data:image/png;base64,{data}"
+    return f"data:image/svg;base64,{data}"
