548c3f96d011dadf27c7bfcf5def6cc2241b565c
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-09 09:41:55 -0500

    Create filters.py

---
+++ evidencelocker/helpers/filters.py
@@ -0,0 +1,18 @@
+import qrcode
+import io
+import base64
+from evidencelocker.__main__ import app
+
+@app.template_filter('qrcode_img_data')
+def qrcode(x):
+  
+    qr = qrcode.QRCode()
+    qr.add_data(x)
+  
+    mem=io.BytesIO
+    img=x.make_image()
+    img.save(mem, format="PNG")
+    mem.seek(0)
+    
+    data=base64.b64encode(mem.read()).decode('ascii')
+    return f"data:image/png;base64,{data}"
