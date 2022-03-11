eaf1f12b9f845b43699e9e148e09e86f5fe3c44f
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-11 09:13:33 -0500

    error pages

---
+++ evidencelocker/routes/errors.py
@@ -0,0 +1,18 @@
+from flask import *
+
+from evidencelocker.__main__ import app
+
+@app.errorhandler(401)
+def error_401(e):
+
+	return render_template("errors/401.html"), 401
+
+@app.errorhandler(403)
+def error_401(e):
+
+	return render_template("errors/403.html"), 403
+
+@app.errorhandler(404)
+def error_401(e):
+
+	return render_template("errors/404.html"), 404
