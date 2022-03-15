ef9d20b419b51b37f7baff75d2196b4ee07119c7
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-15 10:29:11 -0400

    exhibit

---
+++ evidencelocker/helpers/text.py
@@ -0,0 +1,75 @@
+import bleach
+import mistletoe
+
+_allowed_tags = [
+    'a',
+    'b',
+    'blockquote',
+    'br',
+    'code',
+    'del',
+    'em',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'hr',
+    'i',
+    'li',
+    'ol',
+    'p',
+    'pre',
+    'span',
+    'strong',
+    'sub',
+    'sup',
+    'table',
+    'tbody',
+    'th',
+    'thead',
+    'td',
+    'tr',
+    'ul'
+    ]
+
+_allowed_attributes = {
+    'a': ['href', 'target'],
+    'i': [],
+    'span': ['style', 'class', 'data-toggle', 'title'],
+    }
+
+_allowed_protocols = [
+    'http', 
+    'https'
+    ]
+
+_allowed_styles =[
+    'color'
+]
+
+def a_modify(attrs, new=False):
+
+    #callback function that adds target=_blank
+    attrs[(None, "target")] = "_blank"
+
+    return attrs
+
+cleaner=bleach.Cleaner(
+    tags=_allowed_tags,
+    attributes=_allowed_attributes,
+    protocols=_allowed_protocols,
+    filters=[
+        lambda:LinkifyFilter(
+            skip_tags=["pre"],
+            parse_email=False,
+            callbacks=[a_modify]
+            )
+        ]
+    )
+
+
+def raw_to_html(text):
+
+    return cleaner.clean(mistletoe.markdown(text))
--- evidencelocker/routes/exhibits.py
+++ evidencelocker/routes/exhibits.py
@@ -1,19 +1,39 @@
 import bleach
 import mistletoe
+import pyotp
+
 from evidencelocker.decorators.auth import *
+from evidencelocker.helpers.text import raw_to_html
+
 from evidencelocker.__main__ import app
 
 @app.get("/create_exhibit")
 @logged_in_victim
 def get_create_exhibit(user):
 
-	return render_template(
-		"create_exhibit.html",
-		user=user
-		)
+    return render_template(
+        "create_exhibit.html",
+        user=user
+        )
 
 @app.post("/create_exhibit")
 @logged_in_victim
 def post_create_exhibit(user):
 
-	pass
+    title = request.form.get("title")
+
+    body_raw = request.form.get("body")
+
+    body_html = raw_to_html(body_raw)
+
+    signed = request.form.get("oath_perjury", False)
+
+    if signed:
+        if not werkzeug.security.check_password_hash(user.pw_hash, request.form.get("password")) or not pyotp.TOTP(user.otp_secret).verify(request.form.get("otp_code")):
+            return render_template(
+                "create_exhibit.html",
+                user=user,
+                error="Invalid signature",
+                title=title,
+                body=body_raw
+                )
