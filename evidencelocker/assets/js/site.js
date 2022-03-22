d4f961208d5e563355e179ffac6f2fa727ff2416
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-22 11:46:32 -0400

    js buff

--- evidencelocker/assets/js/site.js
+++ evidencelocker/assets/js/site.js
@@ -1,6 +1,7 @@
 // enable/disable signature fields based on perjury oath being selected
 $("#oathcheck").change(function(){
   $('.signature-field').prop('disabled', !$('#oathcheck').prop('checked'));
+  $('#sig-section').toggleClass('text-muted')
   var txt ='Save'
   if ($('#oathcheck').prop('checked')){
     txt='Sign and Save'
