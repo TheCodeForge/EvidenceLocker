8afe5eaea5e2b3838c599d059bc6d7b2814e2b09
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-24 08:38:48 -0400

    author json

--- evidencelocker/routes/lockers.py
+++ evidencelocker/routes/lockers.py
@@ -4,6 +4,7 @@ from evidencelocker.helpers.text import raw_to_html
 from evidencelocker.__main__ import app
 
 @app.get("/locker/<username>")
+@app.get("/locker/<username>.json")
 @logged_in_any
 def get_locker_username(user, username):
 
@@ -11,6 +12,9 @@ def get_locker_username(user, username):
 
 	if not target_user.can_be_viewed_by_user(user):
 		abort(404)
+
+	if request.path.endswith('.json'):
+		return jsonify(target_user.json)
 
 	return render_template(
 		"victim_userpage.html",
