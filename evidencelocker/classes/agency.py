de968140868b1b08abec86636097295c1ffa6701
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-03-28 10:19:06 -0400

    Update agency.py

--- evidencelocker/classes/agency.py
+++ evidencelocker/classes/agency.py
@@ -7,27 +7,27 @@ from evidencelocker.__main__ import Base
 
 class Agency(Base, b36ids, time_mixin):
 
-	__tablename__="agencies"
+    __tablename__="agencies"
 
-	id=Column(Integer, primary_key=True)
-	name=Column(String(256), unique=True)
-	city=Column(String(128))
-	state=Column(String(128))
-	country_code=Column(String(2))
-	domain=Column(String(128), unique=True)
+    id=Column(Integer, primary_key=True)
+    name=Column(String(256), unique=True)
+    city=Column(String(128))
+    state=Column(String(128))
+    country_code=Column(String(2))
+    domain=Column(String(128), unique=True)
 
-	def __repr__(self):
-		return f'<Agency(id={self.id})>'
-
-
-class BadDomain(Base):
-
-	__tablename__="bad_domains"
-
-	id=Column(Integer, primary_key=True)
-	domain=Column(String(128), unique=True)
+    def __repr__(self):
+        return f'<Agency(id={self.id})>'
 
     @property
     def country(self):
         return COUNTRY_CODES[self.country_code]
+
+
+class BadDomain(Base):
+
+    __tablename__="bad_domains"
+
+    id=Column(Integer, primary_key=True)
+    domain=Column(String(128), unique=True)
     
