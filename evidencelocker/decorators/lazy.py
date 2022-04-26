51b283c2de5524359df8193896e86b0e7178f7f4
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-04-26 09:40:02 -0400

    lazy upgrade

--- evidencelocker/decorators/lazy.py
+++ evidencelocker/decorators/lazy.py
@@ -9,10 +9,12 @@ def lazy(f):
         if "_lazy" not in o.__dict__:
             o.__dict__["_lazy"] = {}
 
-        if f.__name__ not in o.__dict__["_lazy"]:
-            o.__dict__["_lazy"][f.__name__] = f(*args, **kwargs)
+        s = f"{f.__name__}_{str(args)}_{str(kwargs)}"
 
-        return o.__dict__["_lazy"][f.__name__]
+        if s not in o.__dict__["_lazy"]:
+            o.__dict__["_lazy"][s]=f(*args, **kwargs)
+
+        return o.__dict__["_lazy"][s]
 
     wrapper.__name__ = f.__name__
     return wrapper
