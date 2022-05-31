343117d8b1c8c157b57b6d6fd47de537ffec7d5c
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-05-31 11:37:05 -0400

    Update blog.py

--- evidencelocker/classes/blog.py
+++ evidencelocker/classes/blog.py
@@ -6,11 +6,11 @@ from evidencelocker.__main__ import Base
 
 class Blog(Base, b36ids, time_mixin):
 
-	id = Column(Integer, primary_key=True)
+    id          =Column(Integer, primary_key=True)
     text_raw    =Column(String(8192))
     text_html   =Column(String(16384))
     created_utc =Column(Integer)
-    author_id	=Column(Integer)
+    author_id   =Column(Integer)
 
 
     author = relationship("AdminUser", lazy="joined", back_populates="blogposts")
