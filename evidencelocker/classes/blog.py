38d51213ab92c55a90ab3b1ba72d9106765e9d12
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-05-31 11:34:41 -0400

    Create blog.py

---
+++ evidencelocker/classes/blog.py
@@ -0,0 +1,16 @@
+from .mixins import *
+
+from sqlalchemy import *
+from sqlalchemy.orm import relationship, lazyload
+from evidencelocker.__main__ import Base
+
+class Blog(Base, b36ids, time_mixin):
+
+	id = Column(Integer, primary_key=True)
+    text_raw    =Column(String(8192))
+    text_html   =Column(String(16384))
+    created_utc =Column(Integer)
+    author_id	=Column(Integer)
+
+
+    author = relationship("AdminUser", lazy="joined", back_populates="blogposts")
